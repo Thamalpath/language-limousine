c6956bb52204d4eb7079685842546f4cd50fc159
Thamalpath Sathimantha <thamalpathsathimantha3@gmail.com>
2024-11-05 12:56:58 +0530

    Modified

--- admin/driver.php
+++ admin/driver.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 'gender' => $gender,
                 'driverId' => $driverId,
                 'vehicle_no' => $vehicle_no,
-                'status' => $status,
+                'status' => $_POST['status'],
                 'driver_id' => $driver_id
             ];
             
