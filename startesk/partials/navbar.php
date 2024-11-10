2e04726dc8e12cd489b9697bed3e18d40fda76a9
Thamalpath Sathimantha <thamalpathsathimantha3@gmail.com>
2024-11-10 17:01:05 +0530

    navbar modified

--- startesk/partials/navbar.php
+++ startesk/partials/navbar.php
@@ -4,10 +4,10 @@
                     <div class="row align-items-center">
                         <div class="col-lg-3 col-md-6">
                             <div class="logo">
-                                <a href="index.html"><img src="img/logo/6.png"
+                                <a href="./"><img src="img/logo/6.png"
                                         class="mobile-logo logo-sticky-none"
                                         alt="Logo"></a>
-                                <a href="index.html"><img src="img/logo/6.png"
+                                <a href="./"><img src="img/logo/6.png"
                                         class="mobile-logo s-logo-none"
                                         alt="Logo"></a>
                             </div>
@@ -18,47 +18,11 @@
                                     <nav id="mobile-menu">
                                         <ul>
                                             <li class="active"><a
-                                                    href="index.html">Home</a>
+                                                    href="./">Home</a>
                                             </li>
-                                            <li><a href="about-us.html">Company</a></li>
-                                            <li><a href="tracking.php">Tracking</a></li>
-                                            <li><a href="#">Pages</a>
-                                                <ul class="submenu">
-                                                    <li><a href="#">Services</a>
-                                                        <ul class="submenu">
-                                                            <li><a
-                                                                    href="service-air.html">Services
-                                                                    Air</a></li>
-                                                            <li><a
-                                                                    href="service-Railway.html">Services
-                                                                    Railway</a></li>
-                                                            <li><a
-                                                                    href="service-door-to-door.html">Services
-                                                                    Door to Door</a></li>
-                                                            <li><a
-                                                                    href="service-warehouse.html">Services
-                                                                    warehouse</a></li>
-                                                        </ul>
-                                                    </li>
-                                                    <li><a href="pricing.html">Pricing
-                                                            Plan</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">News & Media</a>
-                                                <ul class="submenu">
-                                                    <li><a href="blog.html">Blog</a></li>
-                                                    <li><a
-                                                            href="blog-classic.html">Blog
-                                                            Classic</a></li>
-                                                    <li><a
-                                                            href="blog-with-sidebar.html">Blog
-                                                            With Sidebar</a></li>
-                                                    <li><a
-                                                            href="blog-details.html">Blog
-                                                            Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="support.html">Support</a></li>
+                                            <li><a href="about-us">About Us</a></li>
+                                            <li><a href="tracking">Tracking</a></li>
+                                            <li><a href="services">Services</a></li>
                                         </ul>
                                     </nav>
                                 </div>
