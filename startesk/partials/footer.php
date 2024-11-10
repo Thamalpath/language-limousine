f4a49d3e1218e9b27dd0921d0fe4a1a410dc22be
Thamalpath Sathimantha <thamalpathsathimantha3@gmail.com>
2024-11-10 18:30:58 +0530

    Footer Modified

--- startesk/partials/footer.php
+++ startesk/partials/footer.php
@@ -3,109 +3,36 @@
                 data-background="img/bg/footer_bg.jpg">
                 <div class="container">
                     <div class="row">
-                        <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="col-xl-4 col-lg-4 col-md-6">
                             <div class="footer-widget mb-50">
                                 <div class="footer-logo mb-35">
                                     <a href="index.html"><img
-                                            src="img/logo/w_logo.png" alt="img"></a>
+                                        src="img/logo/6.png" alt="img"></a>
                                 </div>
                                 <div class="footer-text">
-                                    <p>Orem Ipsum is simply dumm text the
-                                        printing and types indstr sum has been
-                                        the industry
-                                    </p>
-                                </div>
-                                <div class="footer-social">
-                                    <ul>
-                                        <li><a href="#"><i
-                                                    class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="#"><i
-                                                    class="fab fa-twitter"></i></a></li>
-                                        <li><a href="#"><i
-                                                    class="fab fa-pinterest-p"></i></a></li>
-                                        <li><a href="#"><i
-                                                    class="fab fa-linkedin-in"></i></a></li>
-                                    </ul>
+                                    <p>Language Limousine staff are at the airport daily, ensuring efficient student pickup with our experienced and professional service.</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-xl-3 col-lg-4 col-md-6">
-                            <div class="footer-widget mb-50">
-                                <div class="fw-title mb-30">
-                                    <h5>RECENT POSTS</h5>
-                                </div>
-                                <div class="f-rc-post">
-                                    <ul>
-                                        <li>
-                                            <div class="f-rc-thumb">
-                                                <a href="#"><img
-                                                        src="img/blog/f_rc_img01.jpg"
-                                                        alt="img"></a>
-                                            </div>
-                                            <div class="f-rc-content">
-                                                <span>19 Jun, 2019</span>
-                                                <h5><a href="#">which the syste
-                                                        built and actually</a></h5>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="f-rc-thumb">
-                                                <a href="#"><img
-                                                        src="img/blog/f_rc_img02.jpg"
-                                                        alt="img"></a>
-                                            </div>
-                                            <div class="f-rc-content">
-                                                <span>19 Jun, 2019</span>
-                                                <h5><a href="#">which the syste
-                                                        built and actually</a></h5>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+                        <div class="col-xl-4 col-lg-4 col-md-6">
                         </div>
-                        <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="col-xl-4 col-lg-4 col-md-6">
                             <div class="footer-widget mb-50">
                                 <div class="fw-title mb-30">
                                     <h5>USEFUL LINKS</h5>
                                 </div>
                                 <div class="fw-link">
                                     <ul>
-                                        <li><a href="#"><i
-                                                    class="fas fa-caret-right"></i>
-                                                About us</a></li>
-                                        <li><a href="#"><i
-                                                    class="fas fa-caret-right"></i>
-                                                Delivery Information</a></li>
-                                        <li><a href="#"><i
-                                                    class="fas fa-caret-right"></i>
-                                                Terms & Conditions</a></li>
-                                        <li><a href="#"><i
-                                                    class="fas fa-caret-right"></i>
-                                                Privacy Policy</a></li>
-                                        <li><a href="#"><i
-                                                    class="fas fa-caret-right"></i>
-                                                Refund Policy</a></li>
+                                        <li><a href="./"><i
+                                                class="fas fa-caret-right"></i>
+                                            Home</a></li>
+                                        <li><a href="about-us"><i
+                                                class="fas fa-caret-right"></i>
+                                            About Us</a></li>
+                                        <li><a href="services"><i
+                                                class="fas fa-caret-right"></i>
+                                            Services</a></li>
                                     </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-6">
-                            <div class="footer-widget mb-50">
-                                <div class="fw-title mb-30">
-                                    <h5>Support & Downloads</h5>
-                                </div>
-                                <div class="f-support-content">
-                                    <p>Lorem ipsum dolor sit amet, consy eetur
-                                        adipisc de elit. Quisque act raqum nunc
-                                        no dolor
-                                    </p>
-                                    <a href="#" class="f-download-btn"><img
-                                            src="img/images/f_download_btn01.png"
-                                            alt="img"></a>
-                                    <a href="#" class="f-download-btn"><img
-                                            src="img/images/f_download_btn02.png"
-                                            alt="img"></a>
                                 </div>
                             </div>
                         </div>
@@ -117,14 +44,8 @@
                     <div class="row align-items-center">
                         <div class="col-lg-6 col-md-7">
                             <div class="copyright-text">
-                                <p>Copyright© <span>StarTask </span> | All
+                                <p>Copyright© <span>Language Limousine </span> | All
                                     Rights Reserved</p>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-5">
-                            <div
-                                class="f-payment-method text-center text-md-right">
-                                <img src="img/images/card_img.png" alt="img">
                             </div>
                         </div>
                     </div>
