1fcc69668f9035ce9859d43ccd03a97aa2c6c6e7
Thamalpath Sathimantha <thamalpathsathimantha3@gmail.com>
2024-11-10 16:01:21 +0530

    Break to partials

---
+++ startesk/partials/navbar.php
@@ -0,0 +1,73 @@
+        <header>
+            <div id="header-sticky" class="main-header transparent-header">
+                <div class="container-fluid header-container-p">
+                    <div class="row align-items-center">
+                        <div class="col-lg-3 col-md-6">
+                            <div class="logo">
+                                <a href="index.html"><img src="img/logo/6.png"
+                                        class="mobile-logo logo-sticky-none"
+                                        alt="Logo"></a>
+                                <a href="index.html"><img src="img/logo/6.png"
+                                        class="mobile-logo s-logo-none"
+                                        alt="Logo"></a>
+                            </div>
+                        </div>
+                        <div class="col-lg-9 col-md-6 d-none d-md-block">
+                            <div class="menu-area">
+                                <div class="main-menu">
+                                    <nav id="mobile-menu">
+                                        <ul>
+                                            <li class="active"><a
+                                                    href="index.html">Home</a>
+                                            </li>
+                                            <li><a href="about-us.html">Company</a></li>
+                                            <li><a href="tracking.php">Tracking</a></li>
+                                            <li><a href="#">Pages</a>
+                                                <ul class="submenu">
+                                                    <li><a href="#">Services</a>
+                                                        <ul class="submenu">
+                                                            <li><a
+                                                                    href="service-air.html">Services
+                                                                    Air</a></li>
+                                                            <li><a
+                                                                    href="service-Railway.html">Services
+                                                                    Railway</a></li>
+                                                            <li><a
+                                                                    href="service-door-to-door.html">Services
+                                                                    Door to Door</a></li>
+                                                            <li><a
+                                                                    href="service-warehouse.html">Services
+                                                                    warehouse</a></li>
+                                                        </ul>
+                                                    </li>
+                                                    <li><a href="pricing.html">Pricing
+                                                            Plan</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="#">News & Media</a>
+                                                <ul class="submenu">
+                                                    <li><a href="blog.html">Blog</a></li>
+                                                    <li><a
+                                                            href="blog-classic.html">Blog
+                                                            Classic</a></li>
+                                                    <li><a
+                                                            href="blog-with-sidebar.html">Blog
+                                                            With Sidebar</a></li>
+                                                    <li><a
+                                                            href="blog-details.html">Blog
+                                                            Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="support.html">Support</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile-menu"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
